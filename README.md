f232926bfbac8f359afa4f66dde87c4bbda63467
Andreia Gaita <shana@spoiledcat.net>
2020-10-19 16:44:27 +0200

    The stuff

---
+++ README.md
@@ -0,0 +1,60 @@
+# open-unity.sh
+
+This bash script opens a Unity project from the command line.
+
+It can autodetect what the current build target is for a project, what Unity version is it set to, what Unity versions you have installed on your system, and it will try its best to open a project with the correct settings. Alternatively, you can use it to figure out what a project is set to, if you can't remember (I know I can't). It will ask for confirmation before actually running Unity, and show you the command line it will use to do it, so you can doublecheck things.
+
+I run this on a git bash shell on Windows, and on Terminal (well, iTerm) on macOS. If you get weird errors on mac, make sure the script's line endings are set to LF (Linux), bash no like CRLF outside of Windows.
+
+### Usage
+
+Make sure the script is set to executable (`chmod +x open-unity.sh`) if you want to run it with `./open-unity.sh`, or run it with `sh open-unity.sh` otherwise.
+
+`open-unity.sh [Options] [Build Target] [Batch mode flags] [other flags passed to Unity directly]`
+
+#### Example
+
+`./open-unity.sh -p [Path to Unity Project folder]`
+
+#### Options
+
+```
+    -p|--path [value]             Project path relative to the current directory (optional, current directory by default)
+    -v|--version [value]          Unity version (optional, autodetected by default from project settings)
+    -u [value]                    Path to directory where Unity versions are installed (default: autodetected from Unity hub settings)
+    --unity                       Path to Unity executable (if it really can't find your Unity installation)
+```
+
+#### Currently supported build targets
+
+```
+    -w|--windows                  Set build target to Win64
+    -m|--mac                      Set build target to Mac
+    -a|--android                  Set build target to Android
+    -i|--ios                      Set build target to iOS
+    -x|--xbox                     Set build target to xbox
+    -s|--ps4                      Set build target to ps4
+    -n|--switch                   Set build target to Switch
+```
+
+#### Batch mode flags
+
+```
+    -q|--quit                     Run Unity with -quit flag
+    -b|--batch                    Runs Unity in -batchmode mode. The default method name is BuildHelp.BuildIt_[TARGET]_[CONFIGURATION]. 
+                                  Use -e to set the method name, or -d/-r/-c to use the default name with a specific configuration.
+                                  Implies -q
+
+    -d|--debug                    Used with -b, sets the method to BuildHelp.BuildIt_[TARGET]_Dev
+    -r|--release                  Used with -b, sets the method to BuildHelp.BuildIt_[TARGET]_Release
+    -c|--configuration [value]    Used with -b, sets the method to BuildHelp.BuildIt_[TARGET]_[CONFIGURATION], where CONFIGURATION is what you set here.
+    -e|--method [value]           Run Unity by invoking a method and exiting. Implies -q.
+````
+
+#### Cache server
+
+```
+    -z|--cache [value]            IP or hostname of unity accelerator
+    -v1                           Use cache server v1
+    -v2                           Use cache server v2 (accelerator)
+```
---
+++ open-unity.sh
@@ -0,0 +1,374 @@
+#!/bin/bash -eu
+
+# ------------------------------------------------------------
+# Copyright (c) 2020 Andreia Gaita <shana@spoiledcat.net>
+# Licensed under the MIT License.
+# ------------------------------------------------------------
+
+{ set +x; } 2>/dev/null
+SOURCE=$0
+DIR="$( pwd )"
+
+# unity version, pass -n|--unity to set
+UNITYVERSION=""
+# default cache server, pass --cache to set
+CACHESERVER="cachepi"
+#default cache server version (2 is accelerator)
+CACHEVERSION=2
+# folder where you stuff all your Unitys, pass in --hub to set
+BASEUNITYPATH="/Applications/Unity/Hub/Editor"
+
+HUBPATH=""
+OS="Mac"
+BIN=""
+UNITYTOOLSPATH=""
+
+if [[ -e "/c/" ]]; then
+  OS="Windows"
+  BIN="/usr/bin/"
+  BASEUNITYPATH="C:/Program\ Files/Unity/Hub/Editor"
+  HUBPATH="$( echo ~/AppData/Roaming/UnityHub/ | xargs realpath )" || true
+else
+  HUBPATH="$( pushd ~/Library/Application\ Support/UnityHub/ && pwd && popd )" || true
+fi
+
+if [[ -d $HUBPATH ]]; then
+  HUBPATH="$HUBPATH/secondaryInstallPath.json"
+fi
+
+RM="${BIN}rm"
+CAT="${BIN}cat"
+GREP="${BIN}grep"
+CUT="${BIN}cut"
+DATE="${BIN}date"
+CP="${BIN}cp"
+BIN2TXT="binary2text"
+
+if [[ x"$OS" == x"Windows" ]]; then
+  BIN2TXT="${BIN2TXT}.exe"
+fi
+
+if [[ -f $HUBPATH ]]; then
+
+  tmp="$( $CAT $HUBPATH | sed -r 's, ,\\ ,g' | sed -r 's,",,g' )"
+  if [[ x"$tmp" != x"" ]]; then
+    BASEUNITYPATH="$tmp"
+    if [[ x"$OS" == x"Windows" ]]; then
+      BASEUNITYPATH="$( realpath $BASEUNITYPATH )"
+    fi
+  fi
+fi
+
+CONFIGURATION=Dev
+BATCH=0
+PROJECTPATH="$DIR"
+TARGET=""
+QUIT=0
+METHOD=""
+ARGS=""
+UNITYPATH=""
+
+function usage_platforms() {
+  cat << EOF
+    Build Targets (optional, autodetected from current project settings):
+    -w|--windows                  Set build target to Win64
+    -m|--mac                      Set build target to Mac
+    -a|--android                  Set build target to Android
+    -i|--ios                      Set build target to iOS
+    -x|--xbox                     Set build target to xbox
+    -s|--ps4                      Set build target to ps4
+    -n|--switch                   Set build target to Switch
+EOF
+}
+
+
+function usage() {
+  cat << EOF
+
+    open.sh [Options] [Build Target] [Batch mode flags] [other flags passed to Unity directly]
+
+    Example:
+    ./open.sh -p [Path to Unity Project folder]
+
+    By default it will detect what build target and Unity version the project is currently set to and use that.
+
+    Options:
+    -p|--path [value]             Project path relative to the current directory (optional, current directory by default)
+    -v|--version [value]          Unity version (optional, autodetected by default from project settings)
+    -u [value]                    Path to directory where Unity versions are installed (default: $BASEUNITYPATH)
+    --unity                       Path to Unity executable
+EOF
+
+  usage_platforms
+
+  cat << EOF
+    Batch mode:
+    -q|--quit                     Run Unity with -quit flag
+    -b|--batch                    Runs Unity in -batchmode mode. The default method name is BuildHelp.BuildIt_[TARGET]_[CONFIGURATION]. 
+                                  Use -e to set the method name, or -d/-r/-c to use the default name with a specific configuration.
+                                  Implies -q
+
+    -d|--debug                    Used with -b, sets the method to BuildHelp.BuildIt_[TARGET]_Dev
+    -r|--release                  Used with -b, sets the method to BuildHelp.BuildIt_[TARGET]_Release
+    -c|--configuration [value]    Used with -b, sets the method to BuildHelp.BuildIt_[TARGET]_[CONFIGURATION], where CONFIGURATION is what you set here.
+    -e|--method [value]           Run Unity by invoking a method and exiting. Implies -q.
+
+    Cache server:
+    -z|--cache [value]            IP or hostname of unity accelerator
+    -v1                           Use cache server v1
+    -v2                           Use cache server v2 (accelerator)
+EOF
+}
+
+while (( "$#" )); do
+  case "$1" in
+    -d|--debug)
+      CONFIGURATION="Dev"
+      shift
+    ;;
+    -r|--release)
+      CONFIGURATION="Release"
+      shift
+    ;;
+    -b|--build)
+      BATCH=1
+      QUIT=1
+      shift
+    ;;
+    -c)
+      shift
+      CONFIGURATION=$1
+      shift
+    ;;
+    -v|--version)
+      shift
+      UNITYVERSION="$1"
+      shift
+    ;;
+    -p|--path)
+      shift
+      PROJECTPATH="${DIR}/${1}"
+      shift
+    ;;
+    -x|--xbox)
+      TARGET=XboxOne
+      shift
+    ;;
+    -s|--ps4)
+      TARGET=PS4
+      shift
+    ;;
+    -w|--windows)
+      TARGET=Win64
+      shift
+    ;;
+    -n|--switch)
+      TARGET=Switch
+      shift
+    ;;
+    -a|--android)
+      TARGET=Android
+      shift
+    ;;
+    -m|--mac)
+      TARGET=Mac
+      shift
+    ;;
+    -i|--ios)
+      TARGET=iOS
+      shift
+    ;;
+    -e|--method)
+      shift
+      METHOD=$1
+      QUIT=1
+      shift
+    ;;
+    -z|--cache)
+      shift
+      CACHESERVER="$1"
+      shift
+      ;;
+    -u)
+      shift
+      BASEUNITYPATH="$1"
+      shift
+      ;;
+    --unity)
+      shift
+      UNITYPATH="$1"
+      shift
+      ;;
+    -q|--quit)
+      QUIT=1
+      shift
+    ;;
+    -v1)
+      CACHEVERSION=1
+      shift
+    ;;
+    -v2)
+      CACHEVERSION=2
+      shift
+    ;;
+    --nocache)
+      CACHEVERSION=0
+      shift
+    ;;
+    *)
+    ARGS="$ARGS$(echo $1|xargs) "
+    shift
+    ;;
+  esac
+done
+
+PROJECTPATH="$(echo "$PROJECTPATH" | sed -r 's,/$,,')"
+
+if [[ ! -d "${PROJECTPATH}/Assets" ]]; then
+  usage
+  echo "Error: Invalid path ${PROJECTPATH}" >&2
+  exit 1
+fi
+
+if [[ x"${UNITYPATH}" == x"" ]]; then
+  if [[ x"${UNITYVERSION}" == x"" ]]; then
+    UNITYVERSION="$( $CAT "$PROJECTPATH/ProjectSettings/ProjectVersion.txt" | $GREP "m_EditorVersion:" | $CUT -d' ' -f 2)"
+  fi
+
+  if [[ -d "${BASEUNITYPATH}/${UNITYVERSION}" ]]; then
+    echo "Using Unity v$UNITYVERSION"
+    UNITYPATH="${BASEUNITYPATH}/${UNITYVERSION}"
+  else
+    echo "Error: Unity not found at ${BASEUNITYPATH}/${UNITYVERSION}" >&2
+    echo "Install Unity v$UNITYVERSION or use a different version with -v" >&2
+    exit -1 
+  fi
+
+
+  if [[ x"$OS" == x"Mac" ]]; then
+    UNITYTOOLSPATH="$UNITYPATH/Unity.app/Contents/Tools"
+    UNITYPATH="$UNITYPATH/Unity.app/Contents/MacOS"
+  else
+    UNITYTOOLSPATH="$UNITYPATH/Editor/Data/Tools"
+    UNITYPATH="$UNITYPATH/Editor"
+  fi
+else
+  UNITYTOOLSPATH="$UNITYPATH/Data/Tools"
+fi
+
+if [[ ! -d "${UNITYPATH}" ]]; then
+  usage
+  echo "Error: Unity not found at ${UNITYPATH}" >&2
+  exit 1
+fi
+
+if [[ ! -f "$UNITYTOOLSPATH/$BIN2TXT" ]]; then
+  echo "Error: Unity not found at ${UNITYPATH}" >&2
+  exit 1
+fi
+
+if [[ x"$TARGET" == x"" ]]; then
+
+  EDITORUSERBUILDSETTINGS="$PROJECTPATH/Library/EditorUserBuildSettings.asset"
+  BUILDSETTINGS="$DIR/buildsettings.txt"
+  $RM -f "$BUILDSETTINGS" || true
+
+  if [[ -e "$EDITORUSERBUILDSETTINGS" ]]; then
+
+    "$UNITYTOOLSPATH/$BIN2TXT" "$EDITORUSERBUILDSETTINGS" "$BUILDSETTINGS" || true
+
+    if [[ -e "$BUILDSETTINGS" ]]; then
+
+      ACTIVETARGET="$( $CAT "$BUILDSETTINGS" | $GREP "m_ActiveBuildTarget " | CUT -d' ' -f 2)"
+      $RM -f "$BUILDSETTINGS" || true
+
+      case "$ACTIVETARGET" in
+        2)
+        TARGET=Mac
+        ;;
+        5)
+        TARGET=Win
+        ;;
+        9)
+        TARGET=iOS
+        ;;
+        13)
+        TARGET=Android
+        ;;
+        19)
+        TARGET=Win64
+        ;;
+        31)
+        TARGET=PS4
+        ;;
+        33)
+        TARGET=XboxOne
+        ;;
+        38)
+        TARGET=Switch
+        ;;
+        *)
+        echo "Error: Invalid target $ACTIVETARGET"
+        exit 1
+      esac
+
+    else
+      echo "Error: Project has no active target, pass -w|-s|x to set one" >&2
+      exit 1
+    fi
+
+  fi
+fi
+
+if [[ x"$TARGET" == x"" ]]; then
+  echo "Error: Project has no active target, pass a build target flag to set one" >&2
+  usage_platforms
+  exit 1
+fi
+
+if [[ x"$BATCH" == x"1" && x"$METHOD" == x"" ]]; then
+  METHOD="BuildHelp.BuildIt_${TARGET}_${CONFIGURATION}"
+fi
+
+UNITY_ARGS="-disable-assembly-updater"
+
+if [[ x"$CACHEVERSION" == x"1" ]]; then
+  UNITY_ARGS="${UNITY_ARGS} -CacheServerIPAddress $CACHESERVER"
+elif  [[ x"$CACHEVERSION" == x"2" ]]; then
+  UNITY_ARGS="${UNITY_ARGS} -cacheServerEndpoint $CACHESERVER -adb2 -EnableCacheServer"
+fi
+
+if [[ x"$QUIT" == x"1" ]]; then
+  UNITY_ARGS="${UNITY_ARGS} -quit"
+fi
+
+if [[ x"$BATCH" == x"1" ]]; then
+  UNITY_ARGS="${UNITY_ARGS} -batchmode"
+fi
+
+if [[ x"$METHOD" != x"" ]]; then
+  UNITY_ARGS="${UNITY_ARGS} -executeMethod ${METHOD}"
+fi
+
+UNITY_ARGS="${UNITY_ARGS} ${ARGS}"
+
+LOGFOLDER="$PROJECTPATH/Logs"
+LOGFILE="$LOGFOLDER/Editor.log"
+
+echo "Opening project ${PROJECTPATH} with $UNITYVERSION : $TARGET"
+echo "\"$UNITYPATH/Unity\" -buildTarget $TARGET -projectPath \"$PROJECTPATH\" -logFile \"$LOGFILE\" $UNITY_ARGS &"
+
+read -n1 -r -p "Press space to continue..." key
+
+if [[ x"$key" == x'' ]]; then
+
+  SUBFILENAME=$( $DATE +%Y%m%d-%H%M%S )
+
+  if [[ -e "$LOGFILE" ]]; then
+      $CP "$LOGFILE" "$LOGFOLDER/Editor_$SUBFILENAME.log"
+  fi
+
+  "$UNITYPATH/Unity" -buildTarget $TARGET -projectPath "$PROJECTPATH" -logFile "$LOGFILE" $UNITY_ARGS &
+
+fi
+
